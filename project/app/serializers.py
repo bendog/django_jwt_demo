f042f10df93ac864ad5c78a71f0d15c1ccde1c4a
Ben Fitzhardinge <ben@fitzhardinge.net>
2019-11-29 16:26:32 +0800

    update the serializer to handle create and updates with nested data

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,6 +1,7 @@
-
 from django.contrib.auth.models import User, Group
+from django.db import transaction
 from rest_framework import serializers
+
 from .models import Profile
 
 
@@ -10,6 +11,12 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
@@ -17,11 +24,35 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer(many=False, required=False)
+    profile = UserProfileSerializer(many=False, required=False)
     groups = GroupSerializer(many=True, required=False, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'url', 'username', 'email', 'groups', 'profile']
+        fields = ['id', 'url', 'username', 'email', 'first_name', 'last_name', 'groups', 'profile']
+
+    @transaction.atomic
+    def create(self, validated_data):
+        # remove the profile data
+        profile_data = validated_data.pop('profile')
+        # create user from validated data
+        user = User(**validated_data)
+        # set user password to blank
+        user.set_unusable_password()
+        # save the user
+        user.save()
+        # update the user profile
+        Profile.objects.filter(user=user).update(**profile_data)
+
+    def update(self, instance, validated_data):
+        # remove the profile data
+        profile_data = validated_data.pop('profile')
+        # run the normal update process
+        super().update(instance, validated_data)
+        # handle extra profile information
+        if profile_data:
+            Profile.objects.filter(user=instance).update(**profile_data)
+        return instance
 
 
 class PasswordSerializer(serializers.Serializer):
