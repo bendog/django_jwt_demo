060dc7826ad6ca2e405b56719957c0b65d0da647
Ben Fitzhardinge <ben@fitzhardinge.net>
2019-11-29 09:20:18 +0800

    fix bug with profile name

--- project/app/models.py
+++ project/app/models.py
@@ -3,17 +3,16 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 # Create your models here.
 
 class Profile(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField("Date of birth", auto_now=False, auto_now_add=False)
+    date_of_birth = models.DateField("Date of birth", auto_now=False, auto_now_add=False, null=True)
     nick_name = models.CharField(max_length=50, null=True)
 
-
     def __str__(self):
-        return self.name
+        return self.user.username
 
     # def get_absolute_url(self):
     #     return reverse("profile_detail", kwargs={"pk": self.pk})
